38e0431f323205482af787b9cd2fec3507378351
AleksDzog <asdzog@mail.ru>
2022-08-21 04:21:10 +0300

    echo bot

--- slbot.py
+++ slbot.py
@@ -1,6 +1,33 @@
-def main():
-    print('Hello, world!')
+import dotenv   
+from os.path import join, dirname
+import time
+import os
+import sys
+from aiogram.utils import executor
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+
+
+def get_from_env(key):
+    """Получение секретного ключа из файла."""
+    dotenv_path = join(dirname(__file__), '.env')
+    dotenv.load_dotenv(dotenv_path)
+    return os.environ.get(key)  # Возвращен токен
+
+bot = Bot(token=get_from_env("TELEGRAM_BOT_TOKEN"))
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    """Команда Старт."""
+    await bot.send_message(message.from_user.id, 'Вы нажали кнопку Старт')
+
+
+@dp.message_handler()
+async def text_message(message: types.Message):
+    """Любое текстовое сообщение."""
+    await bot.send_message(message.from_user.id, f'echo: {message.text}')
 
 
 if __name__ == '__main__':
-    main()
+    executor.start_polling(dp)
